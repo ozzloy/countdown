894b5eb1b4481e1e084b12bb311130299de0c04f
daniel watson <ozzloy@each.do>
2024-09-16 07:35:43 -0700

    display current time and current timezone

--- src/to/tuo/app_academy_countdown.cljs
+++ src/to/tuo/app_academy_countdown.cljs
@@ -1,12 +1,30 @@
 (ns to.tuo.app-academy-countdown
-  (:require [goog.dom :as gdom]))
+  (:require [goog.dom :as gdom]
+            [goog.string :as gstring]))
 
-(defn hello-world []
-  (let [app (gdom/getElement "app")]
-    (set! (.-innerHTML app) "<h1>hello, clojurescript</h1>")))
+(defn get-timezone []
+  (.. (js/Intl.DateTimeFormat) (resolvedOptions) -timeZone))
+
+(defn format-time [date]
+  (gstring/format "%02d:%02d:%02d"
+                  (.getHours date)
+                  (.getMinutes date)
+                  (.getSeconds date)))
+
+(defn update-time []
+  (let [app (gdom/getElement "app")
+        current-time (js/Date.)
+        timezone (get-timezone)]
+    (set! (.-innerHTML app)
+          (str "<h1>current time: " (format-time current-time) "</h1>"
+               "<h2>timezone: " timezone))))
+
+(defn start-clock []
+  (js/setInterval update-time 100))
 
 (defn ^:export init []
-  (hello-world))
+  (update-time)
+  (start-clock))
 
 (defn ^:after-load mount []
   (init))
