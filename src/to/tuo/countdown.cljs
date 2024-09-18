7edc8efec48eeba8e7efaf789c9b96120fded061
daniel watson <ozzloy@each.do>
2024-09-17 20:47:05 -0700

    shorten name

---
+++ src/to/tuo/countdown.cljs
@@ -0,0 +1,52 @@
+(ns to.tuo.countdown
+  (:require [goog.dom :as gdom]
+            [goog.string :as gstring]
+            [goog.string.format]))
+
+(defn current-offset [] (.getTimezoneOffset (js/Date.)))
+
+(defn pacific-offset [] 420)
+
+(defn time-until-pacific-time [hour minute]
+  (let [local-now (js/Date.)
+        local-offset (.getTimezoneOffset local-now)
+        pacific-time (doto (js/Date. (.getTime local-now))
+                       (.setHours hour (- (+ minute (pacific-offset))
+                                          local-offset) 0 0))
+        today? (< (.getTime local-now) (.getTime pacific-time))
+        pacific-time (if today?
+                       pacific-time
+                       (doto pacific-time
+                         (.setDate (inc (.getDate pacific-time)))))
+        delta (- (.getTime pacific-time) (.getTime local-now))
+        hours (quot delta (* 1000 60 60))
+        minutes (quot (rem delta (* 1000 60 60)) (* 1000 60))
+        seconds (quot (rem delta (* 1000 60)) 1000)]
+    {:hours hours, :minutes minutes, :seconds seconds}))
+
+(defn format-countdown [{:keys [hours minutes seconds]}]
+  (gstring/format "%02dH%02dM%02dS" hours minutes seconds))
+
+(defn update-countdown [what hour minute]
+  (let [app (gdom/getElement "app")
+        countdown (time-until-pacific-time hour minute)]
+    (set!
+     (.-innerHTML app)
+     (str
+      "<h1>" what ": " (format-countdown countdown) "</h1>"
+
+      "<h1> end of day: "
+      (format-countdown (time-until-pacific-time 17 0))
+      "</h1>"))))
+
+(defn start-clock []
+  (js/setInterval #(update-countdown "class starts" 8 0) 100))
+
+(defn ^:export init []
+  (update-countdown "class starts" 8 0)
+  (start-clock))
+
+(defn ^:after-load mount []
+  (init))
+
+(mount)
